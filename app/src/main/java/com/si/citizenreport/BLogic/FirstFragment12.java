abecf05180f54433c6e91a7e7da95b61a6364a85
Antonio Perez <antonio.perez@jala.university>
2024-06-19 00:28:48 -0600

    Report Done Message

--- app/src/main/java/com/si/citizenreport/BLogic/FirstFragment12.java
+++ app/src/main/java/com/si/citizenreport/BLogic/FirstFragment12.java
@@ -13,8 +13,6 @@ import com.si.citizenreport.R;
 
 
 public class FirstFragment12 extends Fragment {
-    GoogleMap mMap;
-
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -52,8 +50,6 @@ public class FirstFragment12 extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
-
-
         }
     }
 
